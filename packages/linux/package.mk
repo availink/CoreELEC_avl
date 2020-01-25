9b37b984a394ff76b9b2ce6485c96fc0cb369d6a
Arthur Liberman <arthur_liberman@hotmail.com>
2020-01-25 02:13:56 +0200

    linux: bump amlogic-4.9 to fb26c3e

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -26,8 +26,8 @@ case "$LINUX" in
     PKG_BUILD_PERF="no"
     ;;
   amlogic-4.9)
-    PKG_VERSION="cfbd924a0841618dfbcb2062cd22d355891c9d41"
-    PKG_SHA256="21ba07da45e55a9af9123ab25ab50d9e1537da93b1a210f1f97885d74ebc3ebb"
+    PKG_VERSION="fb26c3e7b3ecf523b6d9ec4c3e9d1417edf2c6b2"
+    PKG_SHA256="ba5d1d75b67f9be5488268840cdce05f8f50f32b8dc97d9957b625aef09b6570"
     PKG_URL="https://github.com/CoreELEC/linux-amlogic/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET aml-dtbtools:host"
