030eb34b7c260dff93cd3f94b34e73df5e67759b
tomrich82 <tomrich82@gmail.com>
2020-02-19 14:16:45 -0500

    Tweak dvb-firmware package to work with new amlogic_meson_dvb4linux/linux-firmware-availink

--- packages/linux-firmware/dvb-firmware/package.mk
+++ packages/linux-firmware/dvb-firmware/package.mk
@@ -12,11 +12,11 @@ PKG_URL="https://github.com/CoreELEC/dvb-firmware/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="dvb-firmware: firmwares for various DVB drivers"
 PKG_TOOLCHAIN="manual"
-AVL_PATCH_DIR="${ROOT}/../amlogic_meson_dvb4linux/"
+AVL_FW_DIR="${ROOT}/../amlogic_meson_dvb4linux/linux-firmware-availink/availink"
 
 makeinstall_target() {
   PKG_FW_DIR="$INSTALL/$(get_kernel_overlay_dir)/lib/firmware"
   mkdir -p "$PKG_FW_DIR"
   cp -a "$PKG_BUILD/firmware/"* "$PKG_FW_DIR"
-  cp -a ${AVL_PATCH_DIR}/firmware/availink "$PKG_FW_DIR"
+  cp -a ${AVL_FW_DIR} "$PKG_FW_DIR"
 }
