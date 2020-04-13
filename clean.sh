9299d52563cee82d9a9c490f7ad1410d0d6d4f12
tomrich82 <tomrich82@gmail.com>
2020-04-13 11:24:17 -0400

    update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd build.CoreELEC-Amlogic.arm-9.2-devel/
-rm -rf .stamps/crazycat_aml/ crazycat_aml-* .stamps/media_tree_cc_aml/ media_tree_cc_aml-* .stamps/dvb-firmware/ dvb-firmware-* .stamps/device-trees* device-trees*
+rm -rf .stamps/crazycat_aml/ crazycat_aml-* .stamps/media_tree_cc_aml/ media_tree_cc_aml-* .stamps/dvb-firmware/ dvb-firmware-* .stamps/device-trees* device-trees* .stamps/linux-drivers/ .stamps/linux/ linux-*
 cd ..
