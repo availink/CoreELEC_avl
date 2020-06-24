7f0c9f31fd11d6fefc12344f273d2c56bd15aaa9
tomrich82 <tomrich82@gmail.com>
2020-06-24 10:41:56 -0400

    Add module autoloading Add some magic for telemetry

--- mkfinal.sh
+++ mkfinal.sh
@@ -3,10 +3,13 @@ BOARD=$1
 if [ "$BOARD" == "SDMC" ]; then
   sudo cp SDMC/sdmc_remote.txt /media/$USER/STORAGE/.config/rc_keymaps/
   sudo cp SDMC/rc_maps.cfg /media/$USER/STORAGE/.config/
+  echo "aml_fe_avl62x1_av201x" | sudo tee -a /media/$USER/STORAGE/.config/modules-load.d/aml_fe_avl62x1_av201x.conf
 elif [ "$BOARD" == "vim2" ]; then
   sudo cp vim2/rc_maps.cfg /media/$USER/STORAGE/.config/
+  echo "aml_fe_avl68x2_r848" | sudo tee -a /media/$USER/STORAGE/.config/modules-load.d/aml_fe_avl68x2_r848.conf
 elif [ "$BOARD" == "meCoolK2pro" ]; then
   sudo cp meCool/remote.conf /media/$USER/COREELEC/
+  echo "aml_fe_avl68x2_r848" | sudo tee -a /media/$USER/STORAGE/.config/modules-load.d/aml_fe_avl68x2_r848.conf
 else
   echo "UNKNOWN board '$BOARD'"
   exit 1
@@ -14,4 +17,12 @@ fi
 sudo cp ~/.ssh/id_rsa.pub /media/$USER/STORAGE/.ssh/authorized_keys
 sudo chmod 600 /media/$USER/STORAGE/.ssh/authorized_keys
 sudo cp target/addons/9.2/Amlogic/arm/service.tvheadend42/service.tvheadend42-9.2.*.zip /media/$USER/STORAGE/
+sudo ssh-keygen -f /media/$USER/STORAGE/.ssh/id_rsa -N ""
+sudo cat /media/$USER/STORAGE/.ssh/id_rsa.pub
+pushd ../net_srv
+make arm64
+sudo cp avl_net_srv /media/$USER/STORAGE/.config/
+sudo cp autostart.sh /media/$USER/STORAGE/.config/
+sudo cp telemetry.sh /media/$USER/STORAGE/.config/
+popd
 sync
